c60c167894f95b1bb945a4b1b10979c21a71be08
Mabi Sousa <mabisousacruz@gmail.com>
2022-01-26 15:47:04 -0300

    Componentes

---
+++ src/components/Button/style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+	background: #009394;
+	height: 50px;
+	border-radius: 10px;
+	border: 0;
+	padding: 0 10px;
+	color: #fff;
+	width: 30vw;
+	transition: background-color 0.2s;
+
+	&:hover{
+		background: #006270;
+	}
+`;
---
+++ src/components/Input/style.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  input{
+		width: 250px;
+		height: 50px;
+		background-color: transparent;
+		border: 1px solid white;
+		border-radius: 15px;
+		padding: 16px;
+
+		&::placeholder{
+			background-color: transparent;
+			color: #fff;
+		}
+	}
+`;
