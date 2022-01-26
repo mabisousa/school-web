73b016e9c339d92782b4efe52a50d4c75f8feaad
Mabi Sousa <mabisousacruz@gmail.com>
2022-01-26 14:33:18 -0300

    Cadastro de alunos

---
+++ src/pages/CadastroAluno/index.tsx
@@ -0,0 +1,38 @@
+import React, { useCallback, useRef } from "react";
+import { Container } from './style'
+import api from "../../service/api"
+import { Form } from "@unform/web";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+import { FormHandles } from "@unform/core";
+
+interface AlunoProps{
+	nome: string,
+}
+
+const CadastroAluno: React.FC = () => {
+	const formRef = useRef<FormHandles>(null)
+	const aluno = {
+		nome: "",
+	}
+
+	const cadastrar = useCallback(async (data: AlunoProps) =>{
+		aluno.nome = data.nome;
+		await api.post("/aluno", aluno)
+		window.location.reload();
+	}, [])
+	
+	return (
+		<>
+			<Container>
+				<h1>Cadastrar Aluno</h1> 
+				<Form ref={formRef} onSubmit={cadastrar}>
+					<Input name="nome" placeholder="Informe o nome"/>					
+					<Button type="submit">Cadastrar</Button>
+				</Form> 
+			</Container>
+		</>
+	)
+}
+
+export default CadastroAluno;
