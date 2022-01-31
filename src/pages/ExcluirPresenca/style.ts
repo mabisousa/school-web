afc2eacd62085e98c9121932f3eea5d45af8aaf5
Mabi Sousa <mabisousacruz@gmail.com>
2022-01-31 11:36:59 -0300

    Exclusão de presença - incompleto

---
+++ src/pages/ExcluirPresenca/style.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Icon = styled.a`
+  position: realtive;
+  float: left;
+  margin-top: -100px;
+  margin-left: 80px;
+`;
+
+export const Container = styled.div`
+	height: 70vh;
+  display: flex;
+  flex-direction: column;
+  justify-items: center;  
+  align-items: center;
+  margin-top: 10%;
+
+	Form{
+		height: 10vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		align-content: center;
+	}	
+`;
