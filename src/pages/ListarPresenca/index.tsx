409805597bf792f11efec9fe4f5f949b27e6b944
Mabi Sousa <mabisousacruz@gmail.com>
2022-02-02 14:44:12 -0300

    Junção listagem e exclusão de presença

--- src/pages/ListarPresenca/index.tsx
+++ src/pages/ListarPresenca/index.tsx
@@ -1,39 +1,62 @@
-import React, { useState, useEffect } from "react";
-import { Container, Icon } from "./style";
+import React, { useState, useEffect, useCallback, useRef } from "react";
+import { Container, Header } from "./style";
 import api from "../../service/api";
 import { FiChevronLeft } from 'react-icons/fi';
+import { IoMdAddCircleOutline } from 'react-icons/io';
+import { RiDeleteBinLine } from "react-icons/ri";
+import { Form } from "@unform/web";
+import { FormHandles } from "@unform/core";
 
 interface Presenca{
   id: number,
-  presenca: boolean,
-  aluno:{
-    id: number,
-    nome: string
-  }
+  aluno_id: number,
+  presenca: boolean
+}
+
+interface Aluno{
+  id: number,
+  nome: string
 }
 
 const ListarPresenca: React.FC = () => {
   const [ presencas, setPresenca ] = useState<Presenca[]>([]);
+  const [ alunos, setAluno ] = useState<Aluno[]>([]);
+
+  const formRef = useRef<FormHandles>(null)
 
   useEffect(() => {
     api.get("/presenca").then((response) => {
         setPresenca(response.data);
     })
+
+    api.get("/aluno").then((response) => {
+      setAluno(response.data);
+  })
   }, [])
 
-  if(presencas.map(presenca => presenca.presenca == true)){
-    console.log("verdadeiro")
-  }
+  const excluir = useCallback(async(id) => { 
+    console.log("id >>>", id);
+    await api.delete(`/presenca/${id}`);
+    window.location.reload();
+}, [])
 
   return (
     <>
-      <Icon href="/homePresenca"><FiChevronLeft color="#f4f5f7" size="35px"/></Icon>
+      <Header>
+        <a href="/homePresenca"><FiChevronLeft size="35px"/></a>
+        <h1>Lista de presenças cadastrados</h1>
+        <a href="/cadastroPresenca"><IoMdAddCircleOutline size="35px"/></a>
+      </Header> 
         <Container>
-          <h1>Lista de presenças cadastrados</h1>
           {presencas.map((presenca) => (
-            <div key={presenca.id}>
-              {presenca.presenca == true ? <p>{presenca.aluno.nome} - Presente</p> : <p>{presenca.aluno.nome} - Ausente</p>}
-            </div>
+            <Form key={presenca.id} ref={formRef} onSubmit={() => excluir(presenca.id)}>
+              {presenca.presenca == true ? 
+              <p>Aluno(a): {alunos.map((aluno) => aluno.id == presenca.aluno_id 
+                ? aluno.nome : "")} - Presente </p> 
+              : <p>Aluno(a): {alunos.map((aluno) => aluno.id == presenca.aluno_id 
+                ? aluno.nome : "")} - Ausente</p>}
+              <button type="submit"><RiDeleteBinLine/></button>
+            </Form> 	
           ))}
         </Container>
       </>
--- src/pages/ListarPresenca/style.ts
+++ src/pages/ListarPresenca/style.ts
@@ -1,10 +1,17 @@
 import styled from "styled-components";
 
-export const Icon = styled.a`
-  position: realtive;
-  float: left;
-  margin-top: -100px;
+export const Header = styled.div`
+  width: 90%;
+  height: 5vh;
+  display: flex;
+  margin-top: 37px;
   margin-left: 80px;
+  flex-direction: row;
+  justify-items: center;  
+  justify-content: space-between;
+  svg{
+    color: #f4f5f7;
+  }
 `;
 
 export const Container = styled.div`
@@ -13,12 +20,31 @@ export const Container = styled.div`
   flex-direction: column;
   justify-items: center;  
   align-items: center;
-  margin-top: 10%;
+  margin-top: 37px;
 
-  div{
-    p{
-      color: #dee3e0;
-      margin-top: 15px;
-    }
-  }
+  Form:nth-child(even){
+		background: #009394;
+	}
+
+	Form:nth-child(odd){
+		background: #84a295;
+	}
+
+	Form{
+		width: 90%;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		align-content: center;
+		justify-content: space-between;
+		border-radius: 15px;
+		padding: 15px;
+		margin: 5px;
+
+    button{
+			border: none;
+      background: transparent;
+      color: #fff;
+		}
+	}	
 `
